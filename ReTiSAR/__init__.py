0728c0bef84adcc56b00e34c03ed6f4facac7f6a
HaHeho <hannes.helmholz@chalmers.se>
2020-02-10 18:42:50 +0100

    Revert commit 651852a408f178e27b488625cb8bf0aa4a812197 another time

--- ReTiSAR/__init__.py
+++ ReTiSAR/__init__.py
@@ -4,15 +4,15 @@ __all__ = ['config', 'DataRetriever', 'tools', 'process_logger', 'OscRemote',
            'Compensation', 'Convolver', 'FilterSet', 'HeadTracker', 'Generator',
            'JackPlayer', 'JackRenderer', 'JackGenerator']
 
-from . import *
-from ._compensation import Compensation
-from ._convolver import Convolver
 # IMPORTANT: BE VERY CAUTIOUS IN CHANGING THE ORDER OF IMPORTS HERE !!!
 from ._data_retriever import DataRetriever
+from ._remote import OscRemote
 from ._filter_set import FilterSet
-from ._jack_generator import Generator, JackGenerator
+from ._tracker import HeadTracker
+from ._compensation import Compensation
+from ._convolver import Convolver
 from ._jack_player import JackPlayer
 from ._jack_renderer import JackRenderer
-from ._remote import OscRemote
-from ._tracker import HeadTracker
+from ._jack_generator import Generator, JackGenerator
+from . import *
 # IMPORTANT: BE VERY CAUTIOUS IN CHANGING THE ORDER OF IMPORTS HERE !!!
