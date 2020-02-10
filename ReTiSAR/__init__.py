651852a408f178e27b488625cb8bf0aa4a812197
HaHeho <hannes.helmholz@chalmers.se>
2020-02-10 18:35:39 +0100

    Addition of command line parameter to print version information and exit

--- ReTiSAR/__init__.py
+++ ReTiSAR/__init__.py
@@ -1,18 +1,18 @@
-__version__ = '2020.2.7'
+__version__ = '2020.2.10'
 
 __all__ = ['config', 'DataRetriever', 'tools', 'process_logger', 'OscRemote',
            'Compensation', 'Convolver', 'FilterSet', 'HeadTracker', 'Generator',
            'JackPlayer', 'JackRenderer', 'JackGenerator']
 
-# IMPORTANT: BE VERY CAUTIOUS IN CHANGING THE ORDER OF IMPORTS HERE !!!
-from ._data_retriever import DataRetriever
-from ._remote import OscRemote
-from ._filter_set import FilterSet
-from ._tracker import HeadTracker
+from . import *
 from ._compensation import Compensation
 from ._convolver import Convolver
+# IMPORTANT: BE VERY CAUTIOUS IN CHANGING THE ORDER OF IMPORTS HERE !!!
+from ._data_retriever import DataRetriever
+from ._filter_set import FilterSet
+from ._jack_generator import Generator, JackGenerator
 from ._jack_player import JackPlayer
 from ._jack_renderer import JackRenderer
-from ._jack_generator import Generator, JackGenerator
-from . import *
+from ._remote import OscRemote
+from ._tracker import HeadTracker
 # IMPORTANT: BE VERY CAUTIOUS IN CHANGING THE ORDER OF IMPORTS HERE !!!
